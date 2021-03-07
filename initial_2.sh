fec26bf5262cc9777dc9e37302deadf68eba46f8
RyutaKojima <rkojima0213@gmail.com>
2021-03-07 21:23:47 +0900

    alias setting

--- initial_2.sh
+++ initial_2.sh
@@ -2,3 +2,11 @@
 
 go get github.com/x-motemen/ghq
 
+
+{
+  echo 'alias phpstorm=""'
+  echo 'alias gg="ghq get"'
+  echo 'alias g='\''cd $(ghq list -p | peco)'\'
+  echo 'alias gp='\''g && phpstorm .'\'
+} >> ~/.bashrc
+
